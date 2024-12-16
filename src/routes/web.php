903c08e1a45bf96c232f9766f61412af41562a77
keita <niseyanagitetsuko@gmail.com>
2024-12-16 22:06:14 +0900

    更新

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,9 @@ use App\Http\Controllers\ContactController;
 |
 */
 
+Route::middleware(['auth'])->group(function () {
 Route::get('/admin', [ContactController::class, 'admin'])->name('admin');
+});
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
 Route::post('/contacts', [ContactController::class, 'store']);
